33bc897cfed9f2b24934c6df7c0d1ea439a3d373
Peter A. H. Peterson <pahp@d.umn.edu>
2024-04-24 20:49:03 -0500

    .gitignore

---
+++ listen.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# listen.sh -- listens for a particular type of connection
+# returns true if it gets it or false if it doesn't within n seconds
+
+function usage() {
+
+cat<<EOF
+listen.sh -- listens on a specific port
+listen.sh -p PORT [-u] [-s]
+	-p NUM 	the port on which to listen
+	-u 		use UDP instead of TCP
+	-s SEC	listen for SEC seconds before quitting (default: 10)
+	-h 		prints this screen
+
+Returns true if it received data, otherwise returns false.
+
+EOF
+}
+
+UDP_OPTION=""
+PORT=80
+SEC=10
+OPTIND=""
+
+while getopts "p:s:uh" OPTION
+do
+	case $OPTION in
+		p)
+			PORT=$OPTARG
+			echo "Port is $PORT."
+			;;
+
+		s)
+			SEC=$OPTARG
+			echo "Will wait $SEC seconds before quitting with error."
+			;;
+		u)
+			echo "Protocol is UDP."
+			UDP_OPTION="-u" # null if not enabled
+			;;
+		h)
+			usage
+			exit 0
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
+done
+
+OUTPUT=$(timeout $SEC nc $UDP_OPTION -l $PORT)
+RET=$?
+
+if [[ $RET != 0 ]]
+then
+	echo "Listener timed out."
+fi
+
+if [[ -z $OUTPUT ]]
+then
+	echo "Got no data!"
+	exit 1
+else
+	echo "Got data!"
+	exit 0
+fi
+
+
