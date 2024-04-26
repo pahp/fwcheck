eb7d92384c8449439f4182e7bcd6854c02726bf4
Peter A. H. Peterson <pahp@d.umn.edu>
2024-04-26 02:19:12 +0000

    first functional test

--- checker.sh
+++ checker.sh
@@ -4,22 +4,27 @@
 #
 
 TEMPFILE=$(mktemp)
+SSH_CMD="ssh -o StrictHostKeyChecking=no -i /root/.ssh/fwtest root@client" 
 
 echo "TEMPFILE is $TEMPFILE"
 
-# localhost port 80 is open
-echo "Test 1: localhost port 80 is open."
-./listen.sh -s 10 -p 8000 > $TEMPFILE &
-./talk.sh -H localhost -p 8000
-RET=$?
-if (( $RET > 1 )) 
+if [[ $USER != "root" ]]
 then
-	if grep "Got data!" $TEMPFILE
-	then
-		echo "Test passed!"
-	else
-		echo "RET ok but not output!"
-	fi
-else
-	echo "Test failed!"
+	echo "Please run sudo ./checker.sh"
+	exit 1
 fi
+
+echo "Test to see if client can reach server:443..."
+
+FOO=$($SSH_CMD "curl --insecure -I https://server/")
+
+echo -n "server:443 (tcp) is "
+if [[ -n $FOO ]]
+then
+	echo "accessible from client! :)"
+else
+	echo "not accessible from client! :("
+	exit 1
+fi
+
+
