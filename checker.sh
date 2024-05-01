3ab7966fd3b7a04ccc6eaec2dd7b2708fbaca773
Peter A. H. Peterson <pahp@d.umn.edu>
2024-05-01 20:22:45 +0000

    added return values and scoring

--- checker.sh
+++ checker.sh
@@ -3,12 +3,15 @@
 # runs a series of checks
 #
 
+VERSION=0.5
 DEBUG=false
 RED='\033[0;31m'
 GREEN='\033[1;32m'
 NC='\033[0m'
 SUCCEED=0
 FAIL=1
+TESTS=0
+PASSED=0
 
 function do_test() {
 
@@ -19,7 +22,7 @@ function do_test() {
 	SHOULD_FAIL=$2
 	SSH_HOST="$3"
 	SSH_CMD="$4"
-	PASSED=false
+	RETVAL=false
 
 	$DEBUG && echo "QUIT_ON_FAIL: $QUIT_ON_FAIL SHOULD_FAIL: $SHOULD_FAIL SSH_HOST: $SSH_HOST"
 	$DEBUG && echo "SSH_CMD: $SSH_CMD"
@@ -36,10 +39,10 @@ function do_test() {
 		then
 			# success means connecting
 			echo -e " ${GREEN}It SHOULD work, and it did! SUCCESS :)${NC}"
-			PASSED=true
+			RETVAL=true
 		else
 			echo -e " ${RED}It should work, but it DIDN'T! FAIL :(${NC}"
-			PASSED=false
+			RETVAL=false
 		fi
 	else 
 		# we WANT the connection to FAIL
@@ -48,23 +51,27 @@ function do_test() {
 			# success means NOT connecting
 			echo "OUTPUT: '$OUTPUT' RET: $?"
 			echo -e " ${RED}It should NOT work, but it DID! FAIL! :(${NC}"
-			PASSED=false
+			RETVAL=false
 		else
 			echo -e " ${GREEN}It should NOT work, and it DIDN'T! SUCCESS! :)${NC}"
-			PASSED=true
+			RETVAL=true
 		fi
 	fi
 
-	if [[ ! $PASSED && $QUIT_ON_FAIL == 1 ]]
+	if [[ ! $RETVAL && $QUIT_ON_FAIL == 1 ]]
 	then
 		echo "Quitting."
 		exit 1
 	fi
 
+	$RETVAL # return status of do_test
+
 }
 
 LOG=$(mktemp)
 $DEBUG && echo "Logfile is $LOG"
+
+echo "Starting Firewall Checker v$VERSION..."
 
 # do bootstrap
 for TARGET in client server
@@ -104,47 +111,42 @@ fi
 
 QUIT_ON_FAIL=1
 
-#if [[ $USER != "root" ]]
-#then
-#	echo "Please run sudo ./checker.sh"
-#	exit 1
-#fi
-
-
 echo
 echo Starting tests!
 echo
 
 function client_to_server_tcp443() {
-	echo -n "TEST 1: Can client reach server:443 (tcp) [https]? "
+	echo -n "Test $TESTS: Can client reach server:443 (tcp) [https] (OPTIONAL)? "
 	do_test $QUIT_ON_FAIL 0 client "timeout 5 curl --insecure -I https://server/ 2> /dev/null"
 }
 
 function client_to_server_tcp80() {
-	echo -n "Can client reach server:80 (tcp) [http]..."
+	echo -n "Test $TESTS: Can client reach server:80 (tcp) [http]..."
 	do_test $QUIT_ON_FAIL 0 client "timeout 5 curl http://server/ 2> /dev/null"
 }
 
 function client_to_server_tcp22() {
-	echo -n "Can client reach server:22 (tcp) [ssh]..."
+	echo -n "Test $TESTS: Can client reach server:22 (tcp) [ssh]..."
 	do_test $QUIT_ON_FAIL 0 client "cd fwcheck && ./talk.sh -H server -p 22"
 }
 
 function client_to_server_tcp3306() {
-	echo -n "Can client reach server:3306 (tcp) [mysql] "
+	echo -n "Test $TESTS: Can client reach server:3306 (tcp) [mysql] "
 	do_test $QUIT_ON_FAIL 0 client "cd fwcheck && ./talk.sh -H server -p 3306 -q 2> /dev/null"
 }
 
 
-function client_ping_server() {
-	echo -n "Can client ping server..."
-	do_test $QUIT_ON_FAIL 0 client "ping -W 5 -c 1 server"
-}
 
-function server_ping_client() {
-	echo -n "Can server ping client..."
-	do_test 0 0 server "ping -W -c 1 client"
-	echo "(Technically, the tasks do not require this, but there is a manual test for it.)"
+function source_ping_dest() {
+	SOURCE=$1
+	DEST=$2
+	if [[ -z $SOURCE || -z $DEST ]];
+	then
+		echo "Fatal: Usage: source_ping_dest SOURCE DEST"
+		exit 1
+	fi
+	echo -n "Test $TESTS: Can $SOURCE ping $DEST..."
+	do_test 0 0 server "ping -W 1 -c 1 client"
 }
 
 function both_src_to_dst_proto_port() {
@@ -158,7 +160,7 @@ function both_src_to_dst_proto_port() {
 	PROTO=$4
 	UPORT=$5
 
-	$DEBUG && echo -n "Can $SOURCE reach $DEST on $PROTO port $UPORT... "
+	echo -n "Test $TESTS: Can $SOURCE reach $DEST on $PROTO port $UPORT... "
 
 	if [[ $PROTO == "UDP" || $PROTO == "udp" ]]
 	then
@@ -184,70 +186,94 @@ function src_to_dst_tcp_port() {
 	DEST=$3
 	UPORT=$4
 
-	echo -n "Can $SOURCE reach $DEST on TCP port $UPORT... "
+	echo -n "Test $TESTS: Can $SOURCE reach $DEST on TCP port $UPORT... "
 
 	# start talkers in background with a sleep delay
 	do_test $QUIT_ON_FAIL $DESIRE $SOURCE "cd fwcheck && sudo ./talk.sh -H $DEST -p $UPORT"
 
 }
 
-both_src_to_dst_proto_port $SUCCEED client server "udp" 10000
-exit 
+function inc_tests() {
+	TESTS=$((TESTS + 1))
+}
+
+function inc_passed() {
+	PASSED=$((PASSED + 1))
+}
+
 
 echo "(3.1) Inbound TCP connections to server on standard ports for OpenSSH, Apache, and MySQL:"
-client_to_server_tcp22
-client_to_server_tcp80
-client_to_server_tcp443
-client_to_server_tcp3306
+inc_tests && client_to_server_tcp22 && inc_passed
+inc_tests 
+if client_to_server_tcp80 || client_to_server_tcp443
+then
+	inc_passed
+fi
+inc_tests && client_to_server_tcp3306 && inc_passed
+
+echo "	Some similar connections that should fail:"
+inc_tests && src_to_dst_tcp_port $FAIL client server 23 && inc_passed
+inc_tests && src_to_dst_tcp_port $FAIL client server 25565 && inc_passed
 
 echo
 
 echo "(3.2) Inbound to server ports 10000-10005 on UDP:"
-both_src_to_dst_proto_port $SUCCEED client server "udp" 10000
-both_src_to_dst_proto_port $SUCCEED client server "udp" 10001
-both_src_to_dst_proto_port $SUCCEED client server "udp" 10002
-both_src_to_dst_proto_port $SUCCEED client server "udp" 10003
-both_src_to_dst_proto_port $SUCCEED client server "udp" 10004
-both_src_to_dst_proto_port $SUCCEED client server "udp" 10005
-both_src_to_dst_proto_port $SUCCEED client server "tcp" 10000
-both_src_to_dst_proto_port $FAIL client server "udp" 10006
+inc_tests && both_src_to_dst_proto_port $SUCCEED client server "udp" 10000 && inc_passed
+inc_tests && both_src_to_dst_proto_port $SUCCEED client server "udp" 10001 && inc_passed
+inc_tests && both_src_to_dst_proto_port $SUCCEED client server "udp" 10002 && inc_passed
+inc_tests && both_src_to_dst_proto_port $SUCCEED client server "udp" 10003 && inc_passed
+inc_tests && both_src_to_dst_proto_port $SUCCEED client server "udp" 10004 && inc_passed
+inc_tests && both_src_to_dst_proto_port $SUCCEED client server "udp" 10005 && inc_passed
+
+echo "	Some similar connections that should fail:"
+inc_tests && both_src_to_dst_proto_port $FAIL client server "tcp" 10000 && inc_passed
+inc_tests && both_src_to_dst_proto_port $FAIL client server "udp" 10006 && inc_passed
 
 echo
 
 echo "(3.3) Inbound ICMP pings & replies..."
-client_ping_server
+inc_tests && source_ping_dest client server && inc_passed
 
 echo
 
 echo "(3.3) Outbund ICMP pings & replies..."
-server_ping_client
+echo "(Technically, the tasks do not require this, so it is optional.)"
+source_ping_dest server client
 
 echo
 
-echo "(3.5) Outbound from server to TCP ports 22, 25, 80, 443:"
-src_to_dst_tcp_port $SUCCEED server client 22
-src_to_dst_tcp_port $SUCCEED server client 25
-src_to_dst_tcp_port $SUCCEED server client 80
-src_to_dst_tcp_port $SUCCEED server client 443
+echo "(3.5) Outbound from server to TCP ports 22, 25 and/or 587, 80 and/ or 443:"
+inc_tests && src_to_dst_tcp_port $SUCCEED server client 22 && inc_passed
+
+echo "	Port 25 and/or 587 (only one required):"
+inc_tests
+if both_src_to_dst_proto_port $SUCCEED server client tcp 25 || both_src_to_dst_proto_port $SUCCEED server client tcp 587
+then
+		inc_passed
+fi
+
+echo "	Port 80 and/or 443 (only one required):"
+inc_tests
+if both_src_to_dst_proto_port $SUCCEED server client tcp 80 || both_src_to_dst_proto_port $SUCCEED server client tcp
+then
+	inc_passed
+fi
+
+echo "	Some similar connections that should fail:"
+inc_tests && both_src_to_dst_proto_port $FAIL server client tcp 23 && inc_passed
+inc_tests && both_src_to_dst_proto_port $FAIL server client tcp 25565 && inc_passed
+
 
 echo
 
 echo "(3.6) Outbound from server to client on UDP ports 10006-10010:"
-both_src_to_dst_proto_port $SUCCEED server client udp 10006
-both_src_to_dst_proto_port $SUCCEED server client udp 10007
-both_src_to_dst_proto_port $SUCCEED server client udp 10008
-both_src_to_dst_proto_port $SUCCEED server client udp 10009
-both_src_to_dst_proto_port $SUCCEED server client udp 10010
-both_src_to_dst_proto_port $FAIL server client udp 10000
-both_src_to_dst_proto_port $FAIL server client tcp 10006
+inc_tests && both_src_to_dst_proto_port $SUCCEED server client udp 10006 && inc_passed
+inc_tests && both_src_to_dst_proto_port $SUCCEED server client udp 10007 && inc_passed
+inc_tests && both_src_to_dst_proto_port $SUCCEED server client udp 10008 && inc_passed
+inc_tests && both_src_to_dst_proto_port $SUCCEED server client udp 10009 && inc_passed
+inc_tests && both_src_to_dst_proto_port $SUCCEED server client udp 10010 && inc_passed
+inc_tests && both_src_to_dst_proto_port $FAIL server client udp 10000 && inc_passed
+inc_tests && both_src_to_dst_proto_port $FAIL server client tcp 10006 && inc_passed
 
 echo
-
-echo "Some tests that should not work..."
-both_src_to_dst_proto_port $FAIL client server tcp 1024 
-both_src_to_dst_proto_port $FAIL client server tcp 60000
-both_src_to_dst_proto_port $FAIL client server udp 53
-both_src_to_dst_proto_port $FAIL client server udp 1234
-both_src_to_dst_proto_port $FAIL server client udp 1234
-both_src_to_dst_proto_port $FAIL server client tcp 1234
-both_src_to_dst_proto_port $FAIL server client tcp 23
+echo "Firewall passed $PASSED tests out of $TESTS."
